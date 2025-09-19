265fb90ac4f4d03734234becc2ffd56fb77fc0ab
ii <imanuelchibuzor@gmail.com>
2025-09-19 20:24:49 +0100

    added live frontend url in cors config

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const corsOptions = {
   origin: [
     "http://localhost:3000",
-    "https://www.lead-management-app.vercel.app",
-    "https://lead-management-app.vercel.app",
+    "https://www.lead-app-ruddy.vercel.app/",
+    "https://lead-app-ruddy.vercel.app/",
   ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders:
